8f222aabfc044466b839255d70c631312d386a3c
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-11 16:39:02 -0500

    Full directives example

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
+Vue.component('mini-coding-presentation', {
+  template: '<div><h5>Hey there</h5><p>We are MiniCoding</p></div>'
+})
+
+Vue.component('language-description', {
+  props: ['lan'],
+  template: '<li>Name: {{ lan.name }} -> Rating: {{ lan.rate }}</li>'
+})
+
 var directivesApp = new Vue({
   el: '#directives',
   data: {
-    message: "Hello directives"
+    languages: [
+      {id: 0, name: "Ruby", rate: 9},
+      {id: 1, name: "Java", rate: 8},
+      {id: 2, name: "JavaScript", rate: 10}
+    ]
   }
 })
